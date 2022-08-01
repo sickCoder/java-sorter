dba9cb687625345440845cd01b866d9f13d2d5b0
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-01 19:45:57 +0200

    Add MergeSorterTest class

---
+++ sorters/src/test/java/gibault/alexandre/sorters/MergeSorterTest.java
@@ -0,0 +1,40 @@
+package gibault.alexandre.sorters;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+public class MergeSorterTest {
+
+	@Test
+	public void testMergeSorter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMergeSort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMerge() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMerge_DifferentSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitializeSubArrays() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSortedArray() {
+		fail("Not yet implemented");
+	}
+
+}
