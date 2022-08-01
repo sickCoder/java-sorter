cee39eb4f12f4b454977b18bfdc8da16f497687a
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-01 19:41:27 +0200

    Add fields and not yet implemented methods to MergeSorter

--- sorters/src/main/java/gibault/alexandre/sorters/MergeSorter.java
+++ sorters/src/main/java/gibault/alexandre/sorters/MergeSorter.java
@@ -2,4 +2,25 @@ package gibault.alexandre.sorters;
 
 class MergeSorter {
 
+	int[] arrayToSort;
+	
+	public MergeSorter(int[] arrayToSort) {
+		
+	}
+	
+	/*private*/ void mergeSort() {
+		
+	}
+	
+	/*private*/ void merge() {
+		
+	}
+	
+	/*private*/ void initializeSubArrays(int[] left, int[] right) {
+		
+	}
+	
+	public int[] getSortedArray() {
+		return null;
+	}
 }
