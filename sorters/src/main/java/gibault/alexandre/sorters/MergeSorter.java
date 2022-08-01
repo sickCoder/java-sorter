b62406351ad5f053f0f1b2f7078c7f128db6dcc6
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-01 20:08:33 +0200

    Add MergeSorter documentation

--- sorters/src/main/java/gibault/alexandre/sorters/MergeSorter.java
+++ sorters/src/main/java/gibault/alexandre/sorters/MergeSorter.java
@@ -1,25 +1,72 @@
 package gibault.alexandre.sorters;
 
+/**
+ * This class is intented to sort an <code>int</code> array
+ * using a merge sort algorithm.
+ * 
+ * @author Alexandre Gibault
+ *
+ */
 class MergeSorter {
 
+	/**
+	 * The array we want to Sort
+	 */
 	int[] arrayToSort;
 	
+	/**
+	 * Initialise <code>this.arrayToSort</code> with
+	 * <code>arrayToSort</code>.
+	 * 
+	 * @param arrayToSort The array we want to sort
+	 */
 	public MergeSorter(int[] arrayToSort) {
 		
 	}
 	
+	/**
+	 * Sort <code>arrayToSort</code> using a merge sort
+	 * algorithm.
+	 */
 	/*private*/ void mergeSort() {
 		
 	}
 	
-	/*private*/ void merge() {
+	/**
+	 * Compares <code>left</code> and <code>right</code> elements
+	 * and puts the smaller into <code>arrayToSort</code> assuming that
+	 * <code>left</code> and <code>right</code> are already sorted.
+	 * This means if we have <code>left = {6, 1}</code> and
+	 * <code>right = {3, 4}</code> then 
+	 * <code>arrayToSort = {3, 4, 6, 1}</code>
+	 * 
+	 * @param left left part of <code>arrayToSort</code>
+	 * @param right right part of <code>arrayToSort</code>
+	 */
+	/*private*/ void merge(int[] left, int[] right) {
 		
 	}
 	
+	/**
+	 * Initialize <code>left</code> and <code>right</code>
+	 * with <code>arrayToSort</code> elements. <code>left</code>
+	 * will contain th first half of <code>arrayToSort</code>.
+	 * <code>right</code> will contain the second half of
+	 * <code>arrayToSort</code>.
+	 * 
+	 * @param left the left part of <code>arrayToSort</code>
+	 * @param right the right part of <code>arrayToSort</code>
+	 */
 	/*private*/ void initializeSubArrays(int[] left, int[] right) {
 		
 	}
 	
+	/**
+	 * Returns <code>arrayToSort</code> sorted using a merge sort
+	 * algorithm.
+	 * 
+	 * @return <code>arrayToSort</code> sorted
+	 */
 	public int[] getSortedArray() {
 		return null;
 	}
