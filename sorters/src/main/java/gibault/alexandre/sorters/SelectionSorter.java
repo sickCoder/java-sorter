eb9a3e719144c1f3eb626f21622dcdee466f62b0
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-06 10:14:08 +0200

    Add fields and not yet implemented methods to SelectionSorter

--- sorters/src/main/java/gibault/alexandre/sorters/SelectionSorter.java
+++ sorters/src/main/java/gibault/alexandre/sorters/SelectionSorter.java
@@ -2,4 +2,21 @@ package gibault.alexandre.sorters;
 
 class SelectionSorter {
 
+	private int[] arrayToSort;
+	
+	public SelectionSorter(int[] arrayToSort) {
+		
+	}
+	
+	/*private*/ void sort(int[] arrayToSort) {
+		
+	}
+	
+	/*private*/ void swap(int[] array, int minIndex, int index) {
+		
+	}
+	
+	/*private*/ int[] getSortedArray() {
+		return null;
+	}
 }
