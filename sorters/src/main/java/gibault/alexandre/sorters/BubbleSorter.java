392abd194ccfade984ced8d0b84b050863e74c9d
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-06 15:32:36 +0200

    Add BubbleSorter documentation

--- sorters/src/main/java/gibault/alexandre/sorters/BubbleSorter.java
+++ sorters/src/main/java/gibault/alexandre/sorters/BubbleSorter.java
@@ -1,21 +1,53 @@
 package gibault.alexandre.sorters;
 
+/**
+ * This class is a sorter for an <code>int</code> array
+ * using a bubble sort algorithm.
+ * 
+ * @author Alexandre Gibault
+ *
+ */
 class BubbleSorter {
 
-	private int[] arrayToSort;
+	/**
+	 * The array to sort.
+	 */
+	/*private*/ int[] arrayToSort;
 	
+	/**
+	 * Clone <code>arrayToSort</code> into
+	 * <code>this.arrayToSort</code>.
+	 * And sort <code>this.arrayToSort</code>.
+	 * 
+	 * @param arrayToSort The array to sort
+	 */
 	public BubbleSorter(int[] arrayToSort) {
 		
 	}
 	
+	/**
+	 * Sort <code>this.arrayToSort</code> using a bubble sort
+	 * algorithm.
+	 */
 	/*private*/ void sort() {
 		
 	}
 	
-	/*private*/ void swap(int index1, int index2) {
+	/**
+	 * Swap <code>this.arrayToSort</code> elements at index
+	 * <code>index</code> and <code>index + 1</code>.
+	 * 
+	 * @param index1 The index of the element to swap
+	 */
+	/*private*/ void swap(int index) {
 		
 	}
 	
+	/**
+	 * Return <code>this.arrayToSort</code> sorted.
+	 * 
+	 * @return <code>this.arrayToSort</code> sorted
+	 */
 	public int[] getSortedArray() {
 		return null;
 	}
