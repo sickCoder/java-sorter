78b71a8d410306564eb7fd8e756abf3370198512
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-06 10:33:42 +0200

    Add SelectionSorter documentation

--- sorters/src/main/java/gibault/alexandre/sorters/SelectionSorter.java
+++ sorters/src/main/java/gibault/alexandre/sorters/SelectionSorter.java
@@ -1,21 +1,56 @@
 package gibault.alexandre.sorters;
 
+/**
+ * This class is intended to sort an <code>int</code>
+ * array using a selection sort algorithm.
+ * 
+ * @author Alexandre Gibault
+ *
+ */
 class SelectionSorter {
 
+	/**
+	 * The array to sort.
+	 */
 	private int[] arrayToSort;
 	
+	/**
+	 * Clone <code>arrayToSort</code> into
+	 * <code>this.arrayToSort</code>.
+	 * 
+	 * @param arrayToSort The array to sort
+	 */
 	public SelectionSorter(int[] arrayToSort) {
 		
 	}
 	
+	/**
+	 * Sort <code>arrayToSort</code> with a selection sort
+	 * algorithm.
+	 * 
+	 * @param arrayToSort The array to sort
+	 */
 	/*private*/ void sort(int[] arrayToSort) {
 		
 	}
 	
+	/**
+	 * Swap the <code>minIndex</code> element to
+	 * the <code>index</code> element in <code>array</code>.
+	 * 
+	 * @param array The array to swap elements with
+	 * @param minIndex The index of the minimal value in <code>array</code>
+	 * @param index The index to swap
+	 */
 	/*private*/ void swap(int[] array, int minIndex, int index) {
 		
 	}
 	
+	/**
+	 * Return <code>this.arrayToSort</code> sorted.
+	 * 
+	 * @return <code>this.arrayToSort</code> sorted
+	 */
 	/*private*/ int[] getSortedArray() {
 		return null;
 	}
