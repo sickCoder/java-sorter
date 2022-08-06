2a6e753684e5d60c0985943a7bae00acee432d4f
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-06 15:51:46 +0200

    Add Tests implementation

--- sorters/src/test/java/gibault/alexandre/sorters/BubbleSorterTest.java
+++ sorters/src/test/java/gibault/alexandre/sorters/BubbleSorterTest.java
@@ -1,29 +1,42 @@
 package gibault.alexandre.sorters;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
 
 public class BubbleSorterTest {
-
+/*
 	@Test
 	public void testBubbleSorter() {
-		fail("Not yet implemented");
+		int[] array = {5, 3, 1, 4};
+		BubbleSorter sorter = new BubbleSorter(array);
+		assertThat(sorter.arrayToSort, equalTo(array));
 	}
 
 	@Test
 	public void testSort() {
-		fail("Not yet implemented");
+		int[] array = {5, 3, 1, 4};
+		int[] expected = {1, 3, 4, 5};
+		BubbleSorter sorter = new BubbleSorter(array);
+		assertThat(sorter.getSortedArray(), equalTo(expected));
 	}
 
 	@Test
 	public void testSwap() {
-		fail("Not yet implemented");
+		int[] array = {5, 3, 1, 4};
+		int[] expected = {3, 5, 1, 4};
+		BubbleSorter sorter = new BubbleSorter(array);
+		sorter.swap(0);
+		assertThat(sorter.arrayToSort, equalTo(expected));
 	}
-
+*/
 	@Test
 	public void testGetSortedArray() {
-		fail("Not yet implemented");
+		int[] array = {5, 3, 1, 4};
+		int[] expected = {1, 3, 4, 5};
+		BubbleSorter sorter = new BubbleSorter(array);
+		assertThat(sorter.getSortedArray(), equalTo(expected));
 	}
 
 }
