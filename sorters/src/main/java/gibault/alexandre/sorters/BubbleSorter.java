32486132f33e0ab207cd5ddfb9778f87869f0d2f
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-06 15:23:21 +0200

    Add BubbleSorter field and methods

--- sorters/src/main/java/gibault/alexandre/sorters/BubbleSorter.java
+++ sorters/src/main/java/gibault/alexandre/sorters/BubbleSorter.java
@@ -2,4 +2,21 @@ package gibault.alexandre.sorters;
 
 class BubbleSorter {
 
+	private int[] arrayToSort;
+	
+	public BubbleSorter(int[] arrayToSort) {
+		
+	}
+	
+	/*private*/ void sort() {
+		
+	}
+	
+	/*private*/ void swap(int index1, int index2) {
+		
+	}
+	
+	public int[] getSortedArray() {
+		return null;
+	}
 }
