83c6c47ee0650fa737bebd25b049b7bde83fdbee
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-06 10:35:45 +0200

    Add SelectionSorterTest class

---
+++ sorters/src/test/java/gibault/alexandre/sorters/SelectionSorterTest.java
@@ -0,0 +1,29 @@
+package gibault.alexandre.sorters;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SelectionSorterTest {
+
+	@Test
+	public void testSelectionSorter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSwap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSortedArray() {
+		fail("Not yet implemented");
+	}
+
+}
