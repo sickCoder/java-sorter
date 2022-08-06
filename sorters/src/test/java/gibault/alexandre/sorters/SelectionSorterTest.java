7a98991ad90627dee7e2f1ea452b2103e59d7a64
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-06 10:49:55 +0200

    Add test methods implementation

--- sorters/src/test/java/gibault/alexandre/sorters/SelectionSorterTest.java
+++ sorters/src/test/java/gibault/alexandre/sorters/SelectionSorterTest.java
@@ -1,29 +1,47 @@
 package gibault.alexandre.sorters;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class SelectionSorterTest {
 
 	@Test
 	public void testSelectionSorter() {
-		fail("Not yet implemented");
+		int[] arrayToSort = {5, 3, 1, 4};
+		int[] expected = {1, 3, 4, 5};
+		SelectionSorter sorter = new SelectionSorter(arrayToSort);
+		assertThat(sorter.getSortedArray(), equalTo(expected));
 	}
 
 	@Test
 	public void testSort() {
-		fail("Not yet implemented");
+		int[] arrayToSort = {5, 3, 1, 4};
+		int[] expected = {1, 3, 4, 5};
+		SelectionSorter sorter = new SelectionSorter(arrayToSort);
+		sorter.sort(arrayToSort);
+		assertThat(arrayToSort, equalTo(expected));
 	}
 
 	@Test
 	public void testSwap() {
-		fail("Not yet implemented");
+		int[] array = {5, 3, 1, 4};
+		int[] expected = {1, 3, 5, 4};
+		SelectionSorter sorter = new SelectionSorter(array);
+		sorter.swap(array, 2, 0);
+		assertThat(array, equalTo(expected));
 	}
 
 	@Test
 	public void testGetSortedArray() {
-		fail("Not yet implemented");
+		int[] array = {5, 3, 1, 4};
+		int[] clonedArray = array.clone();
+		int[] expected = {1, 3, 4, 5};
+		SelectionSorter sorter = new SelectionSorter(array);
+		assertThat(sorter.getSortedArray(), equalTo(expected));
+		assertThat(array, equalTo(clonedArray));
 	}
 
 }
