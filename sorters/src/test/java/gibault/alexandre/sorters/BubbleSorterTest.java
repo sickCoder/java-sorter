f437632b7e6a2da491c5a24d918e92443f3128e4
Alexandre Gibault <ag-mainmail@protonmail.com>
2022-08-06 15:24:43 +0200

    Add BubbleSorterTest

---
+++ sorters/src/test/java/gibault/alexandre/sorters/BubbleSorterTest.java
@@ -0,0 +1,29 @@
+package gibault.alexandre.sorters;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BubbleSorterTest {
+
+	@Test
+	public void testBubbleSorter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSwap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSortedArray() {
+		fail("Not yet implemented");
+	}
+
+}
